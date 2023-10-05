ec66fc835e88bd69248759e018ba45a621167fb1
hillary-wangui <hillary.maina@student.moringaschool.com>
2023-10-06 01:20:34 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+// Function to write thank you cards
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  // Function to count down from a given number
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  // Example usage:
+  const names = ["Alice", "Bob", "Charlie"];
+  const eventName = "birthday";
+  const countdownNumber = 5;
+  
+  const thankYouMessages = writeCards(names, eventName);
+  console.log(thankYouMessages);
+  
+  countDown(countdownNumber);
+  
+
+
+
